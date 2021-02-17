25662f173921599984e0e296f704a261cd859247
ggcube <gmggcube@gmail.com>
2021-02-17 21:00:16 +0800

    修正班級數太多產生錯誤

--- seperate.py
+++ seperate.py
@@ -1,25 +1,37 @@
-import openpyxl, os, sys
+import openpyxl, os, sys, datetime
+temp_ver ='1.0' # 樣板版本
 # 讀取原課表檔案
 wb = openpyxl.open(os.path.join('input',os.listdir('input')[0])) 
 sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
-# 開目標 xlsx 物件(或新增)
-if len(os.listdir('output')) == 0:
-    print('\n目標路徑無試算表檔案，啟用新增模式。\n')
-    output_dir = os.path.join('output','class_schedule.xlsx')
-    new_wb = openpyxl.Workbook() # 建立新的活頁簿
-    class_sheet = new_wb.get_sheet_by_name(new_wb.get_sheet_names()[0]) 
-    class_sheet.title = 'class_sheet' # 工作表改名成預設
-elif len(os.listdir('output')) == 1:
-    print('\n找到目標試算表檔案，啟用覆寫模式。\n')
-    output_dir = os.path.join('output',os.listdir('output')[0])
-    new_wb = openpyxl.open(output_dir)
-    class_sheet = new_wb.create_sheet()
-    if 'class_sheet' in new_wb.get_sheet_names():
-        new_wb.remove_sheet(new_wb.get_sheet_by_name('class_sheet')) 
-    class_sheet.title = 'class_sheet' # 工作表改名成預設
-else:
-    print('\n找到2個以上檔案，請移除。\n')
-    sys.exit()
+# 讀取樣板文件
+templete_dir = os.path.join('templete','class_schedule_v'+temp_ver+'.xlsx')
+new_wb = openpyxl.open(templete_dir)
+class_sheet = new_wb.create_sheet()
+if 'class_sheet' in new_wb.get_sheet_names():
+    new_wb.remove_sheet(new_wb.get_sheet_by_name('class_sheet')) 
+class_sheet.title = 'class_sheet' # 工作表改名成預設
+# 生成輸出路徑
+output_dir = os.path.join('output',datetime.datetime.now().strftime('%Y%m%d-%H%M%S')+'.xlsx')
+
+
+# # 開目標 xlsx 物件(或新增)
+# if len(os.listdir('output')) == 0:
+#     print('\n目標路徑無試算表檔案，啟用新增模式。\n')
+#     output_dir = os.path.join('output','class_schedule.xlsx')
+#     new_wb = openpyxl.Workbook() # 建立新的活頁簿
+#     class_sheet = new_wb.get_sheet_by_name(new_wb.get_sheet_names()[0]) 
+#     class_sheet.title = 'class_sheet' # 工作表改名成預設
+# elif len(os.listdir('output')) == 1:
+#     print('\n找到目標試算表檔案，啟用覆寫模式。\n')
+#     output_dir = os.path.join('output',os.listdir('output')[0])
+#     new_wb = openpyxl.open(output_dir)
+#     class_sheet = new_wb.create_sheet()
+#     if 'class_sheet' in new_wb.get_sheet_names():
+#         new_wb.remove_sheet(new_wb.get_sheet_by_name('class_sheet')) 
+#     class_sheet.title = 'class_sheet' # 工作表改名成預設
+# else:
+#     print('\n找到2個以上檔案，請移除。\n')
+#     sys.exit()
 
 
 # 解析課表檔案
